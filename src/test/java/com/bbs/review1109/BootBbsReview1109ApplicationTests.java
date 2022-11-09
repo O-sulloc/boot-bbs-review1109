4cf65f1aabaa4c06a6083d2082140c8490af3a96
O-sulloc <bujjaf@gmail.com>
2022-11-09 11:14:20 +0900

    init

---
+++ src/test/java/com/bbs/review1109/BootBbsReview1109ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbs.review1109;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBbsReview1109ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
